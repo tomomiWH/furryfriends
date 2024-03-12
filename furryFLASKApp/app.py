df70a8eb14f7999f5e5bcdc7b4c51bdc172982bc
tomomiWH <tomomigalaxy@gmail.com>
2024-03-11 21:41:46 -0400

    modified app.py for port

--- furryFLASKApp/app.py
+++ furryFLASKApp/app.py
@@ -19,9 +19,9 @@ app = Flask(__name__)
 
 # database connection info
 app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
-app.config['MYSQL_USER'] = 'cs340_watantom'  #osu username
-app.config['MYSQL_PASSWORD'] = '1710'        #last 4 of db pass   
-app.config['MYSQL_DB'] = 'cs340_watantom'    #osu username
+app.config['MYSQL_USER'] = 'cs340_'  #osu username
+app.config['MYSQL_PASSWORD'] = ''        #last 4 of db pass   
+app.config['MYSQL_DB'] = 'cs340_'    #osu username
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 mysql = MySQL(app)
@@ -802,6 +802,6 @@ def edit_cats_breed_records(cat_breed_record_id):
 # Listener 
 # change the port number if deploying on the flip servers
 if __name__ == "__main__":
-    app.run(port=65315, debug=True)
+    app.run(port=65326, debug=True)
 
 #host="flip2.engr.oregonstate.edu"
